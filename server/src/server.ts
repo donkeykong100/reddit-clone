430956c7d01d5c66468ae308f50b90f155175ccb
donkey_kong <donghui8266@gmail.com>
2022-11-13 22:19:28 +0900

    server: 로그인 구현

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,3 +1,4 @@
+import dotenv from "dotenv";
 import express from "express";
 import morgan from "morgan";
 import { AppDataSource } from "./data-source";
@@ -10,10 +11,13 @@ const origin = "http://localhost:3000";
 app.use(
   cors({
     origin,
+    credentials: true,
   })
 );
 app.use(express.json());
 app.use(morgan("dev"));
+
+dotenv.config();
 
 app.get("/", (_, res) => res.send("running"));
 app.use("/api/auth", authRoutes);
