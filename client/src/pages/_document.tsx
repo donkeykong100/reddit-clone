d9b051fb72246b8c28fb2227e5d9a21293c05e59
donkey_kong <donghui8266@gmail.com>
2022-11-28 15:42:09 +0900

    client: Font Awesome 아이콘 적용을 위한 _document.tsx 파일 작성

---
+++ client/src/pages/_document.tsx
@@ -0,0 +1,20 @@
+import Document, { Head, Html, Main, NextScript } from "next/document";
+
+export default class MyDocument extends Document {
+  render() {
+    return (
+      <Html>
+        <Head>
+          <link
+            rel="stylesheet"
+            href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.8.2/css/all.min.css"
+          />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
