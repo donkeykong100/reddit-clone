f103d4a0a736c6bfbd9a3cfa4a9198ddfe0d85e3
donkey_kong <donghui8266@gmail.com>
2022-11-28 15:44:12 +0900

    server: 로그아웃 핸들러 구현

--- server/src/routes/auth.ts
+++ server/src/routes/auth.ts
@@ -103,9 +103,25 @@ const login = async (req: Request, res: Response) => {
   }
 };
 
+const logout = async (_: Request, res: Response) => {
+  res.set(
+    "Set-Cookie",
+    cookie.serialize("token", "", {
+      httpOnly: true,
+      secure: process.env.NODE_ENV === "production",
+      sameSite: "strict",
+      expires: new Date(0),
+      path: "/",
+    })
+  );
+  res.status(200).json({ success: true });
+};
+
 const router = Router();
 router.get("/me", userMiddleware, authMiddleware, me);
 router.post("/register", register);
 router.post("/login", login);
+// 로그인이 되어있어야 로그아웃할 수 있기 때문에 미들웨어를 거쳐서 실행
+router.post("/logout", userMiddleware, authMiddleware, logout);
 
 export default router;
