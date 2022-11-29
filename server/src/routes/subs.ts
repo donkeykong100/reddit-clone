de7dca47c869aaf52a6cdaa717e1c7dd8a0bfbfb
donkey_kong <donghui8266@gmail.com>
2022-11-29 16:43:09 +0900

    server: sub api 작성

--- server/src/routes/subs.ts
+++ server/src/routes/subs.ts
@@ -8,6 +8,17 @@ import User from "../entities/User";
 import Sub from "../entities/Sub";
 import Post from "../entities/Post";
 
+const getSub = async (req: Request, res: Response) => {
+  const name = req.params.name;
+
+  try {
+    const sub = await Sub.findOneByOrFail({ name });
+    return res.json(sub);
+  } catch (error) {
+    return res.status(404).json({ error: "커뮤니티를 찾을 수 없습니다." });
+  }
+};
+
 const createSub = async (req: Request, res: Response, next: NextFunction) => {
   const { name, title, description } = req.body;
 
@@ -75,6 +86,7 @@ const topSubs = async (_: Request, res: Response) => {
 
 const router = Router();
 
+router.get("/:name", userMiddleware, getSub);
 router.post("/", userMiddleware, authMiddleware, createSub);
 router.get("/sub/topSubs", topSubs);
 
