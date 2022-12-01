29c36e9aa56d6367a7b9d2285cb0502496b9ded8
donkey_kong <donghui8266@gmail.com>
2022-12-01 16:19:37 +0900

    client: 게시물 페이지 포스트, 댓글 부분 UI 및 기능 구현

--- client/src/pages/r/[sub]/[identifier]/[slug].tsx
+++ client/src/pages/r/[sub]/[identifier]/[slug].tsx
@@ -1,18 +1,158 @@
-import axios from "axios";
+import Link from "next/link";
 import { useRouter } from "next/router";
-import React from "react";
+import React, { FormEvent, useState } from "react";
 import useSWR from "swr";
-import { Post } from "../../../../types";
+import { Comment, Post } from "../../../../types";
+import dayjs from "dayjs";
+import { useAuthState } from "../../../../context/auth";
+import axios from "axios";
 
 const PostPage = () => {
   const router = useRouter();
   const { identifier, sub, slug } = router.query;
 
+  const { authenticated, user } = useAuthState();
+
+  const [newComment, setNewComment] = useState("");
+
   const { error, data: post } = useSWR<Post>(
     identifier && slug ? `/posts/${identifier}/${slug}` : null
   );
+  const { data: comments } = useSWR<Comment[]>(
+    identifier && slug ? `/posts/${identifier}/${slug}/comments` : null
+  );
 
-  return <div>PostPage</div>;
+  console.log(comments);
+
+  const handleSubmit = async (event: FormEvent) => {
+    event.preventDefault();
+    if (newComment.trim() === "") return;
+    try {
+      await axios.post(`/posts/${post?.identifier}/${post?.slug}/comments`, {
+        body: newComment,
+      });
+      setNewComment("");
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  return (
+    <div className="flex max-w-5xl px-4 pt-5 mx-auto">
+      <div className="w-full md:mr-3 md:w-8/12">
+        <div className="bg-white rounded">
+          {post && (
+            <>
+              <div className="flex">
+                <div className="py-2 pr-2">
+                  <div className="flex items-center">
+                    <p className="text-xs text-gray-400">
+                      Posted by
+                      <Link
+                        href={`/u/${post.username}`}
+                        className="mx-1 hover:underline"
+                      >
+                        /u/{post.username}
+                      </Link>
+                      <Link href={post.url} className="mx-1 hover:underline">
+                        {dayjs(post.createdAt).format("YYYY-MM-DD HH:mm")}
+                      </Link>
+                    </p>
+                  </div>
+                  <h1 className="my-1 text-xl font-medium">{post.title}</h1>
+                  <p className="my-3 text-sm">{post.body}</p>
+                  <div className="flex">
+                    <button>
+                      <i className="mr-1 fas fa-comment-alt fa-xs"></i>
+                      <span className="font-bold">
+                        {post.commentCount} Comments
+                      </span>
+                    </button>
+                  </div>
+                </div>
+              </div>
+              {/* 댓글 작성 */}
+
+              <div className="pl-10 pr-6 mb-4">
+                {authenticated ? (
+                  <div>
+                    <p className="mb-1 text-xs">
+                      <Link
+                        href={`/u/${user?.username}`}
+                        className="font-semibold text-blue-500"
+                      >
+                        {user?.username}
+                      </Link>{" "}
+                      으로 댓글 작성
+                    </p>
+                    <form onSubmit={handleSubmit}>
+                      <textarea
+                        className="w-full p-3 border border-gray-300 rounded focus:outline-none focus:border-gray-600"
+                        value={newComment}
+                        onChange={({ target: { value } }) =>
+                          setNewComment(value)
+                        }
+                      ></textarea>
+                      <div className="flex justify-end">
+                        <button
+                          id="createComment"
+                          className={`px-3 py-1 text-white  rounded ${
+                            newComment.trim() === ""
+                              ? "bg-gray-200"
+                              : "bg-gray-400 hover:bg-gray-500"
+                          }`}
+                          disabled={newComment.trim() === ""}
+                        >
+                          댓글 작성
+                        </button>
+                      </div>
+                    </form>
+                  </div>
+                ) : (
+                  <div className="flex items-center justify-between px-2 py-4 border border-gray-200 rounded">
+                    <p className="font-semibold text-gray-400">
+                      댓글 작성을 위해서 로그인을 해주세요.
+                    </p>
+                    <div>
+                      <Link
+                        href={`/login`}
+                        className="px-3 py-1 text=white bg-gray-400 rounded"
+                      >
+                        로그인
+                      </Link>
+                    </div>
+                  </div>
+                )}
+              </div>
+              {/* 댓글 리스트 부분 */}
+              {comments?.map((comment) => (
+                <div key={comment.identifier} className="flex">
+                  <div className="py-2 pr-2">
+                    <p className="mb-1 text-xs leading-none">
+                      <Link
+                        href={`/u/${comment.username}`}
+                        className="mr-1 font-bold hover:underline"
+                      >
+                        {comment.username}
+                      </Link>
+                      <span className="text-gray-600">
+                        {`
+                        ${comment.voteScore}
+                        posts
+                        ${dayjs(comment.createdAt).format("YYYY-MM-DD HH:mm")}
+                        `}
+                      </span>
+                    </p>
+                    <p>{comment.body}</p>
+                  </div>
+                </div>
+              ))}
+            </>
+          )}
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default PostPage;
