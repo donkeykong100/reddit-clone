29465a3a65315e88ffd96c46a97904397b064968
donkey_kong <donghui8266@gmail.com>
2022-12-01 17:07:33 +0900

    client: 게시물 및 댓글 vote UI 구현

--- client/src/pages/r/[sub]/[identifier]/[slug].tsx
+++ client/src/pages/r/[sub]/[identifier]/[slug].tsx
@@ -6,6 +6,7 @@ import { Comment, Post } from "../../../../types";
 import dayjs from "dayjs";
 import { useAuthState } from "../../../../context/auth";
 import axios from "axios";
+import classNames from "classnames";
 
 const PostPage = () => {
   const router = useRouter();
@@ -43,6 +44,32 @@ const PostPage = () => {
           {post && (
             <>
               <div className="flex">
+                {/* 좋아요 싫어요 기능 부분 */}
+                <div className="flex-shrink-0 w-10 py-2 text-center rounded-l">
+                  {/* 좋아요 */}
+                  <div
+                    className="w-6 mx-auto text-gray-400 rounded cursor-pointer hover:bg-gray-300 hover:text-red-500"
+                    // onClick={() => vote(1)}
+                  >
+                    <i
+                      className={classNames("fas fa-arrow-up", {
+                        "text-red-500": post.userVote === 1,
+                      })}
+                    ></i>
+                  </div>
+                  <p className="text-xs font-bold">{post.voteScore}</p>
+                  {/* 싫어요 */}
+                  <div
+                    className="w-6 mx-auto text-gray-400 rounded cursor-pointer hover:bg-gray-300 hover:text-blue-500"
+                    // onClick={() => vote(-1)}
+                  >
+                    <i
+                      className={classNames("fas fa-arrow-down", {
+                        "text-blue-500": post.userVote === -1,
+                      })}
+                    ></i>
+                  </div>
+                </div>
                 <div className="py-2 pr-2">
                   <div className="flex items-center">
                     <p className="text-xs text-gray-400">
@@ -126,6 +153,32 @@ const PostPage = () => {
               {/* 댓글 리스트 부분 */}
               {comments?.map((comment) => (
                 <div key={comment.identifier} className="flex">
+                  {/* 좋아요 싫어요 기능 부분 */}
+                  <div className="flex-shrink-0 w-10 py-2 text-center rounded-l">
+                    {/* 좋아요 */}
+                    <div
+                      className="w-6 mx-auto text-gray-400 rounded cursor-pointer hover:bg-gray-300 hover:text-red-500"
+                      // onClick={() => vote(1, comment)}
+                    >
+                      <i
+                        className={classNames("fas fa-arrow-up", {
+                          "text-red-500": comment.userVote === 1,
+                        })}
+                      ></i>
+                    </div>
+                    <p className="text-xs font-bold">{comment.voteScore}</p>
+                    {/* 싫어요 */}
+                    <div
+                      className="w-6 mx-auto text-gray-400 rounded cursor-pointer hover:bg-gray-300 hover:text-blue-500"
+                      // onClick={() => vote(-1, comment)}
+                    >
+                      <i
+                        className={classNames("fas fa-arrow-down", {
+                          "text-blue-500": comment.userVote === -1,
+                        })}
+                      ></i>
+                    </div>
+                  </div>
                   <div className="py-2 pr-2">
                     <p className="mb-1 text-xs leading-none">
                       <Link
